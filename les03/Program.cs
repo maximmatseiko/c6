ce8563090d2eb7b42bfa0128953f1f9f0092fc60
max <maczejko.max@ya.ru>
2022-07-26 17:40:39 +0600

    задача 3

---
+++ les03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+void Method1()
+{
+       Console.Write("Программa, которая задаёт массив из 8 элементов и выводит их на экран =>");
+}
+ Method1();
+ int [] num = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < num.Length; i++)
+ {
+    num [i] = new Random().Next(0, 100);
+    Console.Write(" " + Method ( i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return num[a];
+}
