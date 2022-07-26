4be66b77da58c7184f6c38373cfdeaac343c6520
max <maczejko.max@ya.ru>
2022-07-26 17:20:50 +0600

    задача 2

---
+++ les02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+void method()
+{
+    Console.Write("введите число : ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    while (a > 0)
+    {
+      int num = a % 10;
+       a = a / 10;
+       sum = sum + num;
+    }
+    Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+}
+method();
